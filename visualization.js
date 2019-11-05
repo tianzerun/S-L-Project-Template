0726ff0a793f100e33f1d67e854209d671e71997
Zerun <tian.ze@husky.neu.edu>
2019-11-05 15:09:28 -0500

    full table

--- visualization.js
+++ visualization.js
@@ -6,8 +6,8 @@ d3.csv("/data/survey-data.csv").then(function (data) {
 function tabulate(data, columns) {
     let svg = d3.select("#vis-svg");
     let table = svg.append("foreignObject")
-        .attr("width", 480)
-        .attr("height", 500)
+        .attr("width", 800)
+        .attr("height", '100%')
         .append("xhtml:body");
     let thead = table.append("thead");
     let tbody = table.append("tbody");
@@ -19,6 +19,23 @@ function tabulate(data, columns) {
         .append('th')
         .text(function (column) {
             return column
+        });
+
+    let rows = tbody.selectAll('tr')
+        .data(data)
+        .enter()
+        .append('tr');
+
+    let cells = rows.selectAll('td')
+        .data(function (row) {
+            return columns.map(function (column) {
+                return {column: column, value: row[column]}
+            })
+        })
+        .enter()
+        .append('td')
+        .html(function (d) {
+            return d.value
         });
 
     return table
